9aeb874c54e0105b6c2d1093d34cd0ae8f2a118f
tor-ato <toratokitahara@gmail.com>
2025-06-07 20:41:53 +0900

    [feat] add: clang-tidy

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -11,19 +11,22 @@ class ConfigParser {
         explicit ConfigParser(const ConfigTokenizer& tokenizer);
         ~ConfigParser();
 
-        ConfigTokenizer tokens;
         ConfigNode* getRoot() const;
         static void deleteTree(ConfigNode* node);
         void throwErr(const std::string& str1, const std::string& str2,
                       const int number);
+private:
+		static constexpr int kKeyFlagSize = 16;
+        static constexpr int kMaxLayerDepth = 5;
 
-       private:
-        int keyFlag_[16];
-        ConfigNode* layers_[5];
+        ConfigTokenizer tokens;
+        int keyFlag_[kKeyFlagSize];
+        ConfigNode* root_;
+        ConfigNode* layers_[kMaxLayerDepth];
 
         void makeConfTree_(const ConfigTokenizer& tokens);
-        void updateDepth_(const std::string& token, const int lineNumber);
-        void resetKeyFlag_(const int keyType);
+        void updateDepth_(const std::string& token, int lineNumber);
+        void resetKeyFlag_(int keyType);
         void addChild_(const Token& token, ConfigNode*& current,
                        ConfigNode* parent);
         void setValue_(const Token& token, ConfigNode* node);

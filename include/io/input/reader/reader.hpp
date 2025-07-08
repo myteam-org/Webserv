80d98dce003e1bc4e114fc4a28f3f8663260484d
Yoshika Tsurudome <yotsurud@student.42.fr>
2025-07-08 18:00:28 +0900

    reader.hpp add comment

--- include/io/input/reader/reader.hpp
+++ include/io/input/reader/reader.hpp
@@ -14,3 +14,12 @@ namespace io {
         virtual bool eof() = 0;
     };
 } // namespace io
+
+// IReaderインターフェース
+// ・読み込み可能ななにか（ファイル、ソケットなど）を抽象化したインターフェース
+// ・read()とeof()を必ず実装させる
+// ・read()は最大nbyte分読み、成功か失敗（Result）を返す
+// まとめ
+//  IReader は「何かを読み取れる」共通インターフェース
+//  FdReader はそれを「ファイルディスクリプタで実装したもの」
+//  ReadBuffer は「IReader からバッファ付きで便利に読み取るクラス」

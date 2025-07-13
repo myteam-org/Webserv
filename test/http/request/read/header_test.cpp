8587e70783bc4ba1db24699b426d17148f75cbc3
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-07-13 14:20:52 +0900

    add tester

---
+++ test/http/request/read/header_test.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <cstring>
+#include "io/input/read/buffer.hpp"
+#include "io/input/reader/reader.hpp"
+#include "http/request/read/header.hpp"
+#include "utils/types/result.hpp"
+#include "utils/types/option.hpp"
+#include "utils/types/error.hpp"
+
+namespace {
+
+class DummyReader : public io::IReader {
+public:
+    explicit DummyReader(const std::string& input)
+        : inputData_(input), position_(0) {}
+
+    virtual types::Result<std::size_t, error::AppError> read(char* destination, std::size_t byteCount) {
+        if (position_ >= inputData_.size()) {
+            return types::ok(0ul);
+        }
+        std::size_t copyLength = std::min(byteCount, inputData_.size() - position_);
+        memcpy(destination, inputData_.data() + position_, copyLength);
+        position_ += copyLength;
+        return types::ok(copyLength);
+    }
+
+    virtual bool eof() {
+        return position_ >= inputData_.size();
+    }
+
+private:
+    std::string inputData_;
+    std::size_t position_;
+};
+
+}  // namespace
+
+TEST(ReadingRequestHeadersStateTest, EmptyBufferReturnsSuspend) {
+    DummyReader dummyReader("");
+    ReadBuffer readBuffer(dummyReader);
+    http::ReadingRequestHeadersState state;
+
+    http::TransitionResult result = state.handle(readBuffer);
+
+    ASSERT_TRUE(result.getStatus().isOk());
+    EXPECT_EQ(result.getStatus().unwrap(), http::IState::kSuspend);
+    EXPECT_TRUE(result.getHeaders().isNone());
+}
+
+TEST(ReadingRequestHeadersStateTest, InvalidHeaderReturnsError) {
+    DummyReader dummyReader("InvalidHeader\r\n\r\n");
+    ReadBuffer readBuffer(dummyReader);
+    http::ReadingRequestHeadersState state;
+
+    http::TransitionResult result = state.handle(readBuffer);
+
+    ASSERT_TRUE(result.getStatus().isErr());
+    EXPECT_EQ(result.getStatus().unwrapErr(), error::kIOUnknown);
+    EXPECT_TRUE(result.getHeaders().isNone());
+}
+
+TEST(ReadingRequestHeadersStateTest, ValidHeadersReturnsDone) {
+    DummyReader dummyReader("Host: localhost\r\nUser-Agent: curl\r\n\r\n");
+    ReadBuffer readBuffer(dummyReader);
+    http::ReadingRequestHeadersState state;
+
+    http::TransitionResult result = state.handle(readBuffer);
+
+    ASSERT_TRUE(result.getStatus().isOk());
+    EXPECT_EQ(result.getStatus().unwrap(), http::IState::kDone);
+
+    ASSERT_TRUE(result.getHeaders().isSome());
+    const RawHeaders& headers = result.getHeaders().unwrap();
+
+    ASSERT_EQ(headers.size(), 2);
+    EXPECT_EQ(headers.find("Host")->second, " localhost");
+    EXPECT_EQ(headers.find("User-Agent")->second, " curl");
+}
+
+TEST(ReadingRequestHeadersStateTest, HeaderWithoutColonReturnsError) {
+    DummyReader dummyReader("Host localhost\r\n\r\n");
+    ReadBuffer readBuffer(dummyReader);
+    http::ReadingRequestHeadersState state;
+
+    http::TransitionResult result = state.handle(readBuffer);
+
+    ASSERT_TRUE(result.getStatus().isErr());
+    EXPECT_TRUE(result.getHeaders().isNone());
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

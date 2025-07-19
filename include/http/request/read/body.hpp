65e89c7c833f75d2d5a4193fe1411b4bc3f9bcd6
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-07-19 17:36:08 +0900

    create ReadingRequestBodyState class

---
+++ include/http/request/read/body.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "buffer.hpp"
+#include "state.hpp"
+#include "length_body.hpp"
+#include "chunked_body.hpp"
+
+namespace http {
+
+enum BodyEncodingType {
+    kNone,
+    kContentLength,
+    kChunked
+};
+
+class ReadingRequestBodyState : public IState {
+   public:
+    explicit ReadingRequestBodyState(BodyEncodingType type, const BodyLengthConfig& config);
+    virtual ~ReadingRequestBodyState();
+    virtual TransitionResult handle(ReadBuffer& buf);
+
+   private:
+    IState* activeBodyState_;     // ★ 本体：Length or Chunked を指す
+    http::BodyEncodingType type_; // ★ デバッグやロジック判断に残してOK
+    BodyLengthConfig config_;     // ★ LengthState に渡す用の初期値
+};
+}  // namespace http

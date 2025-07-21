3f98d15ac763a67a683bbb72f3a4d3cfa831b108
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-07-21 17:53:24 +0900

    fix read.cpp handle()

--- src/http/request/read/body.cpp
+++ src/http/request/read/body.cpp
@@ -19,38 +19,17 @@ ReadingRequestBodyState::ReadingRequestBodyState(BodyEncodingType type, const Bo
     // }
 }
 
-ReadingRequestBodyState::~ReadingRequestBodyState() {}
+ReadingRequestBodyState::~ReadingRequestBodyState() {
+    delete activeBodyState_;
+}
 
 TransitionResult ReadingRequestBodyState::handle(ReadContext& ctx, ReadBuffer& buf) {
     TransitionResult tr;
-    RawHeaders headers;
-
-    while (true) {
-        const GetLineResult result = getLine(buf);
-        if (!result.canUnwrap()) {
-            tr.setStatus(types::err(result.unwrapErr()));
-            return tr;
-        }
-        const types::Option<std::string> lineOpt = result.unwrap();
-        if (lineOpt.isNone()) {
-            tr.setStatus(types::ok(kSuspend));
-            return tr;
-        }
-        const std::string line = lineOpt.unwrap();
-        if (line.empty()) {
-            tr.setHeaders(types::some(headers));
-            tr.setStatus(types::ok(kDone));
-            return tr;
-        }
-        const std::string::size_type colon = line.find(':');
-        if (colon == std::string::npos) {
-            tr.setStatus(types::err(error::kIOUnknown));
-            return tr;
-        }
-        const std::string key = line.substr(0, colon);
-        const std::string value = line.substr(colon + 1);
-        headers.insert(std::make_pair(key, value));
+    if (activeBodyState_ == NULL) {
+        tr.setStatus(types::err(error::kIOUnknown));
+        return tr;
     }
+    return activeBodyState_->handle(ctx, buf);
 }
 
 }  // namespace http

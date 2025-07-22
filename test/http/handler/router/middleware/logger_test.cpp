e31f2afff22a5cdbd75430b810618072a42b8bbe
tor-ato <toratokitahara@gmail.com>
2025-07-22 14:36:22 +0900

    [78] done: log_test

---
+++ test/http/handler/router/middleware/logger_test.cpp
@@ -0,0 +1,36 @@
+
+#include "http/handler/router/middleware/logger.hpp"
+#include "gtest/gtest.h"
+#include "http/handler/handler.hpp"
+#include "http/request/request.hpp"
+#include "http/response/response.hpp"
+#include "utils/types/either.hpp"
+#include "http/method.hpp"
+#include "utils/types/option.hpp"
+#include "http/status.hpp"
+
+namespace http {
+
+// Dummy IHandler for testing
+class DummyHandler : public IHandler {
+public:
+    // Test function
+    virtual Either<IAction*, Response> serve(const Request& request) {
+        (void)request;
+        Response response(http::kStatusOk, types::none<std::string>(), "");
+        return Right<Response>(response);
+    }
+};
+
+TEST(LoggerTest, Intercept) {
+    Logger logger;
+    DummyHandler dummy_handler;
+    Request request(http::kMethodGet, "/test", "HTTP/1.1", "");
+
+    Either<IAction*, Response> result = logger.intercept(request, dummy_handler);
+
+    ASSERT_TRUE(result.isRight());
+    EXPECT_EQ(result.unwrapRight().getStatusCode(), http::kStatusOk);
+}
+
+}

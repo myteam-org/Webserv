10f9181fa1edcc141d416ccf2052fe89b1632f48
tor-ato <toratokitahara@gmail.com>
2025-07-25 19:17:58 +0900

    [94] redirect: compile ok

--- src/http/handler/file/static.cpp
+++ src/http/handler/file/static.cpp
@@ -1,3 +1,5 @@
+#include "http/response/builder.hpp"
+#include "builder.hpp"
 #include "static.hpp"
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -59,23 +61,23 @@ StaticFileHandler::makeDirectoryListingHtml(const std::string &rootPath, const s
 
 static Response buildFileResponse(const struct stat &fileStatus, const std::string &filePath) {
     if (!S_ISREG(fileStatus.st_mode)) {
-        // return ResponseBuilder().status(kStatusForbidden).build();
+        return ResponseBuilder().status(kStatusForbidden).build();
     }
-    // return ResponseBuilder().file(filePath).build();
+    return ResponseBuilder().file(filePath).build();
 }
 
 Response StaticFileHandler::directoryListing(const std::string &rootPath, const std::string &targetPath) {
     const types::Result<std::string, HttpStatusCode> htmlResult = makeDirectoryListingHtml(rootPath, targetPath);
     if (htmlResult.isErr()) {
-        // return ResponseBuilder().status(htmlResult.unwrapErr()).build();
+        return ResponseBuilder().status(htmlResult.unwrapErr()).build();
     }
-    // return ResponseBuilder().html(htmlResult.unwrap()).build();
+    return ResponseBuilder().html(htmlResult.unwrap()).build();
 }
 
 Response StaticFileHandler::handleDirectory(const Request &request, const std::string &directoryPath) const {
 
     if (!utils::endsWith(request.getRequestTarget(), "/")) {
-        // return ResponseBuilder().redirect(request.getRequestTarget() + "/").build();
+        return ResponseBuilder().redirect(request.getRequestTarget() + "/").build();
     }
 
     const std::string indexFilePath = directoryPath + documentRootConfig_.getIndex();
@@ -89,10 +91,10 @@ Response StaticFileHandler::handleDirectory(const Request &request, const std::s
     }
 
     if (errno == ENOENT || errno == EACCES) {
-        // return ResponseBuilder().status(kStatusForbidden).build();
+        return ResponseBuilder().status(kStatusForbidden).build();
     }
 
-    // return ResponseBuilder().status(kStatusInternalServerError).build();
+    return ResponseBuilder().status(kStatusInternalServerError).build();
 }
 
 Response StaticFileHandler::serveInternal(const Request &request) const {
@@ -101,12 +103,12 @@ Response StaticFileHandler::serveInternal(const Request &request) const {
     struct stat fileStatus = {};
     if (stat(filePath.c_str(), &fileStatus) == -1) {
         if (errno == ENOENT) {
-            // return ResponseBuilder().status(kStatusNotFound).build();
+            return ResponseBuilder().status(kStatusNotFound).build();
         }
         if (errno == EACCES) {
-            // return ResponseBuilder().status(kStatusForbidden).build();
+            return ResponseBuilder().status(kStatusForbidden).build();
         }
-        // return ResponseBuilder().status(kStatusInternalServerError).build();
+        return ResponseBuilder().status(kStatusInternalServerError).build();
     }
 
     if (S_ISDIR(fileStatus.st_mode)) {

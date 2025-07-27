0a9dd8b473e9ecb5b87fca6a5238432791d1f59b
tor-ato <toratokitahara@gmail.com>
2025-07-27 17:17:31 +0900

    [94] ref: copilot review

--- src/http/mime.cpp
+++ src/http/mime.cpp
@@ -1,40 +1,53 @@
 #include "mime.hpp"
 #include <algorithm>
 #include <map>
+#include <string>
+#include <cstddef>
 
 namespace http {
-    namespace {
-        std::map<std::string, std::string> createMimeTypeMap() {
-            std::map<std::string, std::string> mimeTypes;
-            mimeTypes["html"] = "text/html";
-            mimeTypes["htm"] = "text/html";
-            mimeTypes["css"] = "text/css";
-            mimeTypes["js"] = "application/javascript";
-            mimeTypes["json"] = "application/json";
-            mimeTypes["png"] = "image/png";
-            mimeTypes["jpg"] = "image/jpeg";
-            mimeTypes["jpeg"] = "image/jpeg";
-            mimeTypes["txt"] = "text/plain";
-            return mimeTypes;
-        }
-    } // namespace
 
-    std::string getMimeType(const std::string &fileName) {
-        const static std::map<std::string, std::string> mimeTypes = createMimeTypeMap();
+namespace {
 
-        const std::size_t lastDot = fileName.find_last_of('.');
-        if (lastDot == std::string::npos) {
-            return "application/octet-stream";
-        }
+const std::pair<const char*, const char*> mimeTypePairs[] = {
+    std::make_pair("html", "text/html"),
+    std::make_pair("htm", "text/html"),
+    std::make_pair("css", "text/css"),
+    std::make_pair("js", "application/javascript"),
+    std::make_pair("json", "application/json"),
+    std::make_pair("png", "image/png"),
+    std::make_pair("jpg", "image/jpeg"),
+    std::make_pair("jpeg", "image/jpeg"),
+    std::make_pair("txt", "text/plain")
+};
 
-        std::string extension = fileName.substr(lastDot + 1);
-        std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+std::map<std::string, std::string> createMimeTypeMap() {
+    std::map<std::string, std::string> mimeTypes;
+    const std::size_t mimeTypeCount = sizeof(mimeTypePairs) / sizeof(mimeTypePairs[0]);
+    for (std::size_t index = 0; index < mimeTypeCount; ++index) {
+        mimeTypes[mimeTypePairs[index].first] = mimeTypePairs[index].second;
+    }
+    return mimeTypes;
+}
 
-        const std::map<std::string, std::string>::const_iterator it = mimeTypes.find(extension);
-        if (it != mimeTypes.end()) {
-            return it->second;
-        }
+} // namespace
 
+std::string getMimeType(const std::string &fileName) {
+    const static std::map<std::string, std::string> mimeTypes = createMimeTypeMap();
+
+    const std::size_t lastDotPosition = fileName.find_last_of('.');
+    if (lastDotPosition == std::string::npos) {
         return "application/octet-stream";
     }
-} //namespace http
+
+    std::string fileExtension = fileName.substr(lastDotPosition + 1);
+    std::transform(fileExtension.begin(), fileExtension.end(), fileExtension.begin(), ::tolower);
+
+    const std::map<std::string, std::string>::const_iterator found = mimeTypes.find(fileExtension);
+    if (found != mimeTypes.end()) {
+        return found->second;
+    }
+
+    return "application/octet-stream";
+}
+
+} // namespace http

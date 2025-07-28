8a6f6c3428f759581a790f6f44e76643e7985356
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-07-29 00:55:36 +0900

    add AppError

--- include/utils/types/error.hpp
+++ include/utils/types/error.hpp
@@ -5,5 +5,12 @@ namespace error {
         kIOUnknown,
         kBadRequest,
         kRequestEntityTooLarge,   // ← 追加：413 Payload Too Large
+        kBadMethod,
+        kBadHttpVersion,
+        kMissingHost,
+        kInvalidContentLength,
+        kInvalidTransferEncoding,
+        kHasContentLengthAndTransferEncoding,
+        kContainNonDigit,
     };
 } // namespace error

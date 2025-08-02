04f6f6e68d670630599e548811479b09c0bec0f0
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-08-02 16:31:17 +0900

    fix request

--- include/http/request/request.hpp
+++ include/http/request/request.hpp
@@ -1,37 +1,56 @@
 #pragma once
 
+// #include "serverContext.hpp"
+// #include "locationContext.hpp"
 #include "option.hpp"
 #include "method.hpp"
+#include "raw_headers.hpp"
 #include <map>
 #include <vector>
 #include <string>
 
+class ServerContext;
+class LocationContext;
+class DocumentRootConfig;
 
 namespace http {
-  typedef std::vector<std::string> RawHeaders;
   class Request {
   public:
     explicit Request(
       HttpMethod method,
       const std::string &requestTarget,
-      const std::string &httpVersion = "HTTP/1.1",
-      // const Headers &headers = Headers(),
-      const std::string &body = ""
+      const std::string &pathOnly,
+      const std::string &queryString,
+      const std::string &httpVersion,
+      const RawHeaders &headers,
+      const std::vector<char> &body,
+      const ServerContext *server,
+      const LocationContext *location
     );
 
     bool operator==(const Request &other) const;
 
     HttpMethod getMethod() const;
     const std::string &getRequestTarget() const;
+    const std::string& getPath() const;
+    const std::string& getQueryString() const;
     const std::string &getHttpVersion() const;
-    static types::Option<std::string> getHeader(const std::string &key);
-    const std::string &getBody() const;
+    const types::Option<std::string> getHeader(const std::string &key);
+    const std::vector<char> &getBody() const;
+    const ServerContext* getServer() const;
+    const LocationContext* getLocation() const;
+    const DocumentRootConfig* getDocumentRoot() const;
 
   private:
     HttpMethod method_;
     std::string requestTarget_;
+    std::string pathOnly_;
+    std::string queryString_;
     std::string httpVersion_;
-    // Headers headers_;
-    std::string body_;
+    RawHeaders headers_;
+    std::vector<char> body_;
+    const ServerContext* server_;
+    const LocationContext* location_;
+    const DocumentRootConfig* documentRoot_;
   };
 } // namespace http
--- src/http/request/request.cpp
+++ src/http/request/request.cpp
@@ -1,38 +1,53 @@
 #include "http/request/request.hpp"
+#include "config/context/locationContext.hpp"
 
 namespace http {
-    Request::Request(
-        HttpMethod method,
-        const std::string &requestTarget,// NOLINT(bugprone-easily-swappable-parameters)
-        const std::string &httpVersion,
-        const std::string &body
-    ) : method_(method), requestTarget_(requestTarget), httpVersion_(httpVersion), body_(body) {}
+Request::Request(HttpMethod method, const std::string &requestTarget,
+                 const std::string &pathOnly, const std::string &queryString,
+                 const std::string &httpVersion, const RawHeaders &headers,
+                 const std::vector<char> &body, const ServerContext *server,
+                 const LocationContext *location)
+    : method_(method),
+      requestTarget_(requestTarget),
+      pathOnly_(pathOnly),
+      queryString_(queryString),
+      httpVersion_(httpVersion),
+      headers_(headers),
+      body_(body),
+      server_(server),
+      location_(location) {
+    documentRoot_ = &location_->getDocumentRootConfig();
+}
 
-    bool Request::operator==(const Request &other) const {
-        return method_ == other.method_ &&
-               requestTarget_ == other.requestTarget_ &&
-               httpVersion_ == other.httpVersion_ &&
-               body_ == other.body_;
-    }
+bool Request::operator==(const Request &other) const {
+    return method_ == other.method_ && requestTarget_ == other.requestTarget_ &&
+           httpVersion_ == other.httpVersion_ && body_ == other.body_;
+}
 
-    HttpMethod Request::getMethod() const {
-        return method_;
-    }
+HttpMethod Request::getMethod() const { return method_; }
 
-    const std::string &Request::getRequestTarget() const {
-        return requestTarget_;
-    }
+const std::string &Request::getRequestTarget() const { return requestTarget_; }
 
-    const std::string &Request::getHttpVersion() const {
-        return httpVersion_;
-    }
+const std::string &Request::getPath() const { return pathOnly_; }
 
-    types::Option<std::string> Request::getHeader(const std::string &key) {
-        (void)key; // Unused parameter
+const std::string &Request::getQueryString() const { return queryString_; }
+
+const std::string &Request::getHttpVersion() const { return httpVersion_; }
+
+const types::Option<std::string> Request::getHeader(const std::string &key) {
+    for (RawHeaders::const_iterator it = headers_.begin(); it != headers_.end(); ++it) {
+        if (it != headers_.end()) {
+            return types::some(it->second);
+        }
         return types::none<std::string>();
     }
+}
 
-    const std::string &Request::getBody() const {
-        return body_;
-    }
-} // namespace http
+const std::vector<char>& Request::getBody() const { return body_; }
+
+const ServerContext* Request::getServer() const { return server_; }
+
+const LocationContext* Request::getLocation() const { return location_; }
+
+const DocumentRootConfig* Request::getDocumentRoot() const { return documentRoot_; }
+}  // namespace http

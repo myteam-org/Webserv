f3c4fe6ed4230cc5be1227b51ffb8fba69424e3b
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-08-10 23:17:12 +0900

    changed virtual_server.cpp

--- src/http/virtual_server.cpp
+++ src/http/virtual_server.cpp
@@ -1,9 +1,29 @@
 #include "virtual_server.hpp"
-#include "router/router.hpp"
+
 #include "handler/router/builder.hpp"
 #include "middleware/chain.hpp"
+#include "router/router.hpp"
+#include "router/middleware/logger.hpp"
+#include "router/middleware/error_page.hpp"
+#include "http/handler/file/redirect.hpp"
+#include "http/handler/file/static.hpp"
+#include "http/handler/file/upload.hpp"
+#include "http/handler/file/delete.hpp"
+#include "http/handler/file/cgi.hpp"
 
-VirtualServer::VirtualServer(const ServerContext &serverConfig, const std::string &bindAddress)
+static ErrorPageMap MergeErrorPages(const std::vector<std::map<int, std::string> >& vec) {
+    ErrorPageMap out;
+
+    for (std::vector<std::map<int, std::string> >::const_iterator it = vec.begin(); it != vec.end(); ++it) {
+        for (std::map<int, std::string>::const_iterator jt = it->begin(); jt != it->end(); ::jt) {
+            out.insert(it->begin(), it->end()); 
+        }
+    }
+    return out;
+}
+
+VirtualServer::VirtualServer(const ServerContext &serverConfig,
+                             const std::string &bindAddress)
     : serverConfig_(serverConfig), bindAddress_(bindAddress), router_(NULL) {
     this->setupRouter();
 }
@@ -18,56 +38,66 @@ const ServerContext &VirtualServer::getServerConfig() const {
     return serverConfig_;
 }
 
-http::Router &VirtualServer::getRouter() {
-    return *router_;
-}
+http::Router &VirtualServer::getRouter() { return *router_; }
 
-void VirtualServer::registerHandlers(http::RouterBuilder &routerBuilder, const LocationContext &locationContext) {
-    (void)routerBuilder; // 未使用パラメータ警告対策 FileHandler系実装時に削除
-    const std::vector<http::HttpMethod>& allowedHttpMethods = locationContext.getAllowedMethods();
-    for (std::vector<http::HttpMethod>::const_iterator methodIterator = allowedHttpMethods.begin();
-         methodIterator != allowedHttpMethods.end();
-         ++methodIterator) {
-        const DocumentRootConfig documentRootConfig = locationContext.getDocumentRootConfig(); // NOLINT
-        // TODO(FileHandler)
-        switch (*methodIterator) {
-            case http::kMethodGet: { // NOLINT(bugprone-branch-clone)
-                // http::IHandler *getHandler = new http::StaticFileHandler(documentRootConfig);
-                // routerBuilder.route(http::kMethodGet, locationContext.getPath(), getHandler);
-                break;
-            }
-            case http::kMethodDelete: {
-                // http::IHandler *deleteHandler = new http::DeleteFileHandler(documentRootConfig);
-                // routerBuilder.route(http::kMethodDelete, locationContext.getPath(), deleteHandler);
-                break;
-            }
-            default:
-                // 何もしない
-                break;
+// ビルド時に “各 location を順に登録する” 関数
+void VirtualServer::registerHandlers(http::RouterBuilder &routerBuilder,
+                                     const LocationContext &locationContext) {
+    (void)routerBuilder;  // 未使用パラメータ警告対策 FileHandler系実装時に削除
+    const DocumentRootConfig& docRoot = locationContext.getDocumentRootConfig();
+    const std::string& path = locationContext.getPath();
+    const OnOff* allowed = locationContext.getAllowedMethod();
+
+    const bool isUploadLocation = (path == "/upload");
+    const bool cgiEnabled = (docRoot.getCgiExtensions() == ON);
+
+    if (isUploadLocation && allowed[POST] == ON) {
+        routerBuilder.route(http::kMethodPost, path, new http::UploadFileHandler(docRoot));
+    }
+    if (!isUploadLocation && cgiEnabled) {
+        if (allowed[GET] == ON) { // TODO
+            // routerBuilder.route(http::kMethodGet, path, new http::CgiHandler(docRoot));
         }
+        if (allowed[POST] == ON) { // TODO
+            // routerBuilder.route(http::kMethodPost, path, new http::CgiHandler(docRoot));
+        }
+        return ;
+    }
+
+    if (allowed[GET] == ON) {
+        routerBuilder.route(http::kMethodGet, path, new http::StaticFileHandler(docRoot));
+    }
+    if (allowed[POST] == ON) {
+        routerBuilder.route(http::kMethodPost, path, new http::UploadFileHandler(docRoot));
+    }
+    if (allowed[DELETE] == ON) {
+        routerBuilder.route(http::kMethodDelete, path, new http::DeleteFileHandler(docRoot));
     }
 }
 
 void VirtualServer::setupRouter() {
     http::RouterBuilder routerBuilder;
-    const LocationContextList locationContextList = serverConfig_.getLocation();
-    for (LocationContextList::const_iterator locationIterator = locationContextList.begin();
-         locationIterator != locationContextList.end();
-         ++locationIterator) {
-        const LocationContext &locationContext = *locationIterator;
-        // if (locationContext.getRedirect().isSome()) {
-            // http::IHandler *redirectHandler = new http::RedirectHandler(locationContext.getRedirect().unwrap());
-            // routerBuilder.route(locationContext.getAllowedMethods(), locationContext.getPath(), redirectHandler);
-        // } else {
+    const std::vector<LocationContext> &locations = serverConfig_.getLocation();
+    for (LocationContextList::const_iterator it = locations.begin();
+         it != locations.end(); ++it) {
+        const LocationContext &locationContext = *it;
+        if (!locationContext.getRedirect().empty()) {
+            http::IHandler *redirectHandler =
+                new http::RedirectHandler(locationContext.getRedirect());
+            routerBuilder.route(locationContext.getAllowedMethods(),
+                                locationContext.getPath(), redirectHandler);
+        } else {
             registerHandlers(routerBuilder, locationContext);
-        // }
+        }
     }
 
-    // routerBuilder.middleware(new http::Logger());
-    // routerBuilder.middleware(new http::ErrorPage(serverConfig_.getErrorPage()));
+        routerBuilder.middleware(new http::Logger());
+        const ErrorPageMap merged = MergeErrorPages(serverConfig_.getErrorPage());
+        routerBuilder.middleware(new
+        http::ErrorPage(merged));
 
-    if (router_ != NULL) {
-        delete router_;
-    }
-    router_ = routerBuilder.build();
+        if (router_ != NULL) {
+            delete router_;
+        }
+        router_ = routerBuilder.build();
 }

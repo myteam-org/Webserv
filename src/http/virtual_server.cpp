772f2519cc7d18b1368371ee353400a70160cf5b
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-08-12 17:32:56 +0900

    restore registerHandlers()

--- src/http/virtual_server.cpp
+++ src/http/virtual_server.cpp
@@ -1,29 +1,16 @@
 #include "virtual_server.hpp"
 
 #include "handler/router/builder.hpp"
-#include "middleware/chain.hpp"
-#include "router/router.hpp"
-#include "router/middleware/logger.hpp"
-#include "router/middleware/error_page.hpp"
+#include "http/handler/file/cgi.hpp"
+#include "http/handler/file/delete.hpp"
 #include "http/handler/file/redirect.hpp"
 #include "http/handler/file/static.hpp"
 #include "http/handler/file/upload.hpp"
-#include "http/handler/file/delete.hpp"
-#include "http/handler/file/cgi.hpp"
 #include "http/status.hpp"
-
-// namespace {
-// ErrorPageMap mergeErrorPages(const std::map<http::HttpStatusCode, std::string>>& vec) {
-//     ErrorPageMap out;
-
-//     for (std::map<http::HttpStatusCode, std::string>>::const_iterator it = vec.begin(); it != vec.end(); ++it) {
-//         for (std::map<int, std::string>::const_iterator jt = it->begin(); jt != it->end(); ++jt) {
-//             out[static_cast<http::HttpStatusCode>(jt->first)] = jt->second; 
-//         }
-//     }
-//     return out;
-// }
-// } // namespace
+#include "middleware/chain.hpp"
+#include "router/middleware/error_page.hpp"
+#include "router/middleware/logger.hpp"
+#include "router/router.hpp"
 
 VirtualServer::VirtualServer(const ServerContext &serverConfig,
                              const std::string &bindAddress)
@@ -46,44 +33,46 @@ http::Router &VirtualServer::getRouter() { return *router_; }
 // ビルド時に “各 location を順に登録する” 関数
 void VirtualServer::registerHandlers(http::RouterBuilder &routerBuilder,
                                      const LocationContext &locationContext) {
-    const DocumentRootConfig& docRoot = locationContext.getDocumentRootConfig();
+    const DocumentRootConfig &docRoot = locationContext.getDocumentRootConfig();
     const std::string& path = locationContext.getPath();
-    const OnOff* allowed = locationContext.getAllowedMethod();
-
-    const bool isUploadLocation = (path == "/upload");
+    const OnOff *allowed = locationContext.getAllowedMethod();
     const bool cgiEnabled = (docRoot.getCgiExtensions() == ON);
 
-    if (isUploadLocation && allowed[POST] == ON) {
-        routerBuilder.route(http::kMethodPost, path, new http::UploadFileHandler(docRoot));
-    }
-    if (!isUploadLocation && cgiEnabled) {
+    if (cgiEnabled) {
         if (allowed[GET] == ON) {
-            // routerBuilder.route(http::kMethodGet, path, new http::CgiHandler(docRoot));
+            // TODO
+            // routerBuilder.route(http::kMethodGet, path, new
+            // http::CgiHandler(docRoot));
         }
         if (allowed[POST] == ON) {
-            // routerBuilder.route(http::kMethodPost, path, new http::CgiHandler(docRoot));
+            // TODO
+            // routerBuilder.route(http::kMethodPost, path, new
+            // http::CgiHandler(docRoot));
         }
         return;
     }
 
     if (allowed[GET] == ON) {
-        routerBuilder.route(http::kMethodGet, path, new http::StaticFileHandler(docRoot));
+        routerBuilder.route(http::kMethodGet, path,
+                            new http::StaticFileHandler(docRoot));
     }
     if (allowed[POST] == ON) {
-        routerBuilder.route(http::kMethodPost, path, new http::UploadFileHandler(docRoot));
+        routerBuilder.route(http::kMethodPost, path,
+                            new http::UploadFileHandler(docRoot));
     }
     if (allowed[DELETE] == ON) {
-        routerBuilder.route(http::kMethodDelete, path, new http::DeleteFileHandler(docRoot));
+        routerBuilder.route(http::kMethodDelete, path,
+                            new http::DeleteFileHandler(docRoot));
     }
 }
 
 void VirtualServer::setupRouter() {
     http::RouterBuilder routerBuilder;
-    const std::vector<LocationContext> &locations = serverConfig_.getLocation();
+    const LocationContextList locationContextList = serverConfig_.getLocation();
 
-    for (LocationContextList::const_iterator it = locations.begin();
-         it != locations.end(); ++it) {
-        const LocationContext &locationContext = *it;
+    for (LocationContextList::const_iterator locationIterator = locationContextList.begin();
+         locationIterator != locationContextList.end(); ++locationIterator) {
+        const LocationContext &locationContext = *locationIterator;
         if (!locationContext.getRedirect().empty()) {
             http::IHandler *redirectHandler =
                 new http::RedirectHandler(locationContext.getRedirect());

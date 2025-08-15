57bf45ea4948a14494a83ff38291978d802ade37
Yoshika Tsurudome <yoshi.tsurudome@gmail.com>
2025-08-16 01:49:37 +0900

    create Headers class

---
+++ src/http/response/response_headers.cpp
@@ -0,0 +1,39 @@
+#include "response_headers.hpp"
+#include "utils/string.hpp"
+
+namespace http {
+
+    Headers::Headers() : member_() {}
+    Headers::Headers(const ResponseHeaderFields& init) : member_(init) {}
+    Headers::~Headers() {}
+
+
+    void Headers::setField(const std::string& name, const std::string& value) {
+        const std::string key = utils::toLower(name);
+        member_[key] = value;
+    }
+
+    bool Headers::hasField(const std::string& name) const {
+        const std::string key = utils::toLower(name);
+        return member_.find(key) != member_.end();
+    }
+
+    const std::string& Headers::getField(const std::string& name) const {
+        static const std::string kEmpty;
+        const std::string key = utils::toLower(name);
+        ResponseHeaderFields::const_iterator it = member_.find(key);
+        if (it == member_.end()) {
+            return kEmpty;
+        }
+        return it->second;
+    }
+
+    const ResponseHeaderFields& Headers::getMembers() const {
+        return member_;
+    }
+
+    bool Headers::removeField(const std::string& name) {
+        return member_.erase(utils::toLower(name)) != 0;
+    }
+
+} // namespace http
